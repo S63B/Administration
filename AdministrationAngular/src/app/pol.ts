1676f125a08070c4155329f1b358549d3e61996c
binq1000 <t.daniels@student.fontys.nl>
2017-04-18 13:45:08 +0200

    Removed items from RoadPricing that are not needed.

--- AdministrationAngular/src/app/pol.ts
+++
@@ -1,7 +0,0 @@
-export class Pol {
-    constructor(
-        public id: number,
-        public lat: number,
-        public lng: number) {
-    }
-}
--- AdministrationAngular/src/app/ride.ts
+++
@@ -1,9 +0,0 @@
-import { Pol } from './pol';
-
-export class Ride {
-    constructor(
-        public id: number,
-        public pols: Pol[],
-        public distance: number) {
-    }
-}
