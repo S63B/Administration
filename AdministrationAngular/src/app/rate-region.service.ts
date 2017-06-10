83b5ff792bad901185b03a983932f77d20fbe5ba
Nino Vrijman <ninovrijman@gmail.com>
2017-06-10 10:09:50 +0200

    Two way data binding, create and update functions setup

--- AdministrationAngular/src/app/rate-region.service.ts
+++ AdministrationAngular/src/app/rate-region.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from "app/http.service";
+import { RateRegion } from "app/rate-region";
 
 @Injectable()
 export class RateRegionService {
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService
+  ) {
+
+  }
+
+  /**
+   * Gets all existing rate regions from the database.
+   * 
+   * @memberof RateRegionService
+   */
+  getAll() {
+    return this.httpService.get(`/rate`);
+  }
+
+  /**
+   * Creates a new rate region in the database through the server.
+   * 
+   * @param {RateRegion} rateRegion The rate region that should be created.
+   *
+   * @memberof RateRegionService
+   */
+  create(newRateRegion: RateRegion) {
+    const rr = newRateRegion;
+    return this.httpService.put(`/rate/${rr.id}/${rr.lat}/${rr.lng}/${rr.rate}/${rr.startDate}/${rr.endDate}`);
+  }
+
+  /**
+   * * Updates an existing rate region in the database through the server
+   * 
+   * @param {RateRegion} updatedRateRegion The rate region that needs to be updated.
+   * 
+   * @memberof RateRegionService
+   */
+  update(updatedRateRegion: RateRegion) {
+    const rr = updatedRateRegion; 
+    return this.httpService.put(`/rate/${rr.id}/${rr.lat}/${rr.lng}/${rr.rate}/${rr.startDate}/${rr.endDate}`);
+  }
 
 }
--- AdministrationAngular/src/app/rateregions/rateregions.component.ts
+++ AdministrationAngular/src/app/rateregions/rateregions.component.ts
@@ -11,34 +11,72 @@ import { RateRegionService } from "app/rate-region.service";
   styleUrls: ['./rateregions.component.css']
 })
 export class RateRegionsComponent implements OnInit {
-  private rateRegions: RateRegion[];
   private defaultID: number = -1;
   private defaultRate: number = 1.35;
   private defaultRadius: number = 500;
-  private defaultStartDate: string = '09-06-2017';
-  private defaultEndDate: string = '31-12-2017';
+  private defaultStartDate: string = '2017-06-09';
+  private defaultEndDate: string = '2017-12-31';
+
+  private rateRegions: RateRegion[];
 
   constructor(
     private uuidService: UUIDService,
     private rateRegionService: RateRegionService
   ) {
     this.rateRegions = [];
-    this.rateRegions.push(new RateRegion(-1, "asdfkajsdflka;sdf", 10, 51.436596, 5.478001, 500, 'nu', 'morgen'));
+    this.rateRegions.push(new RateRegion(-1, "asdfkajsdflka;sdf", 10, 51.436596, 5.478001, 500, this.defaultStartDate, this.defaultEndDate));
+    this.getRateRegions();
   }
 
   ngOnInit() {
-    // get regions, create unique uuid per region
   }
 
+  /**
+   * Gets all the existing rate regions.
+   * 
+   * @memberof RateRegionsComponent
+   */
   getRateRegions() {
-
+    this.rateRegionService.getAll().subscribe(rateRegions => {
+      console.log(rateRegions);
+    });
   }
 
-  saveRateRegion(rateRegion: RateRegion) {
-    console.log(rateRegion);
+  /**
+   * Creates a new rate region in the database through the server.
+   * 
+   * @param {RateRegion} rateRegion The rate region that should be created.
+   * 
+   * @memberof RateRegionsComponent
+   */
+  createNewRateRegion(rateRegion: RateRegion) {
+    this.rateRegionService.create(rateRegion).subscribe(createdRateRegion => {
+      // update object by using rateRegion.uuid
+    });
   }
 
-  mapClicked(mapClickedEvent: any) {
+  /**
+   * Updates an existing rate region in the database through the server.
+   * 
+   * @param {RateRegion} rateRegion The rate region that needs to be updated.
+   * 
+   * @memberof RateRegionsComponent
+   */
+  updateRateRegion(rateRegion: RateRegion) {
+    this.rateRegionService.update(rateRegion).subscribe(updatedRateRegion => {
+
+    });
+    // this.rateRegionService.update(rateRegion)
+  }
+
+  /**
+   * Event handler for when a right click occurs on the map.
+   * 
+   * @param {any} mapClickedEvent An event object containing the coordinates of where the map was right clicked.
+   * 
+   * @memberof RateRegionsComponent
+   */
+  mapRightClicked(mapClickedEvent: any) {
     const coordinates = mapClickedEvent.coords;
     const newUUID = this.generateUUID();
     const newRegion 
@@ -46,7 +84,15 @@ export class RateRegionsComponent implements OnInit {
     this.rateRegions.push(newRegion);
   }
 
-  radiusChanged(newRadius: any, changedRateRegion: RateRegion) {
+  /**
+   * Event handler for when a rate regions radius has been changed.
+   * 
+   * @param {number} newRadius The new radius in meters.
+   * @param {RateRegion} changedRateRegion The rate region of which the radius has been changed.
+   * 
+   * @memberof RateRegionsComponent
+   */
+  radiusChanged(newRadius: number, changedRateRegion: RateRegion) {
     const index = this.rateRegions.findIndex(region => region.uuid === changedRateRegion.uuid);
     if (index === -1) {
       throw new Error(`Rate region couldn't be found in rate regions array`);
@@ -55,11 +101,18 @@ export class RateRegionsComponent implements OnInit {
     if (foundRateRegion.radius !== newRadius) {
       foundRateRegion.radius = newRadius;
       this.rateRegions[index] = foundRateRegion;
-      console.log('radius changed');
     }
   }
 
-  centerChanged(newCenterCoordinates: any, changedRateRegion: RateRegion) {
+  /**
+   * Event handler for when the centerpoint of a rate region has been changed.
+   * 
+   * @param {any} newCenterCoordinates An object containing the new latitude and longitude of a rate regino.
+   * @param {RateRegion} changedRateRegion The rate region of which the centerpoint has been changed.
+   * 
+   * @memberof RateRegionsComponent
+   */
+  centerpointChanged(newCenterCoordinates: any, changedRateRegion: RateRegion) {
     const index = this.rateRegions.findIndex(region => region.uuid === changedRateRegion.uuid);
     if (index === -1) {
       throw new Error(`Rate region couldn't be found in rate regions array`);
@@ -72,6 +125,14 @@ export class RateRegionsComponent implements OnInit {
     }
   }
 
+  /**
+   * Generates a random UUID which is used to identify rate regions which do not have a database id assigned to them.
+   * Theres a slight change that generated values might not be unique.
+   * 
+   * @returns {string} A random UUID.
+   * 
+   * @memberof RateRegionsComponent
+   */
   generateUUID(): string {
     let uuid = this.uuidService.generate();
     while (this.doesUUIDExist(uuid)) {
@@ -80,9 +141,15 @@ export class RateRegionsComponent implements OnInit {
     return uuid;
   }
 
+  /**
+   * Checks if the generate UUID is already being used by one of the rgions.
+   * 
+   * @param {string} uuid 
+   * @returns {boolean} Whether the given UUID is already in used by another region.
+   * 
+   * @memberof RateRegionsComponent
+   */
   doesUUIDExist(uuid: string) {
     return typeof this.rateRegions.find(region => region.uuid === uuid) !== 'undefined';
   }
-
-  save
 }
